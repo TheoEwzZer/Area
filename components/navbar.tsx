530eed1e7fc5871d792b5e29333f3717ce638893
Théo <theo.fabianomattei@gmail.com>
2024-09-17 15:54:03 +0200

    ♻️ refactor: update navbar component to include full height class in html tag

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,16 +1,25 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { ReactElement } from "react";
 import { Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { usePathname } from "next/navigation";
 
-export function Navbar(): ReactElement {
+export function Navbar(): ReactElement | null {
+  const pathname: string = usePathname();
+
+  if (pathname === "/sign-in" || pathname === "/sign-up") {
+    return null;
+  }
+
   return (
     <header className="sticky top-0 flex h-16 items-center gap-4 border-b px-4 md:px-6 bg-white">
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
         <Link
-          href="#"
+          href="/create"
           className="flex items-center gap-2 text-lg font-semibold md:text-base"
         >
           <span className="text-2xl font-bold">AREA</span>
@@ -19,13 +28,13 @@ export function Navbar(): ReactElement {
           href="/my-applets"
           className="text-foreground transition-colors hover:text-foreground"
         >
-            My Applets
+          My Applets
         </Link>
         <Link
           href="/create"
           className="text-muted-foreground transition-colors hover:text-foreground"
         >
-            Create
+          Create
         </Link>
       </nav>
       <Sheet>
@@ -42,7 +51,7 @@ export function Navbar(): ReactElement {
         <SheetContent side="left">
           <nav className="grid gap-6 text-lg font-medium">
             <Link
-              href="#"
+              href="/create"
               className="flex items-center gap-2 text-lg font-semibold"
             >
               <span className="text-2xl font-bold">AREA</span>
@@ -60,11 +69,11 @@ export function Navbar(): ReactElement {
         </SheetContent>
       </Sheet>
       <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-        <div className="ml-auto flex-1 sm:flex-initial"/>
+        <div className="ml-auto flex-1 sm:flex-initial" />
         <SignedOut>
           <SignInButton mode="modal">
             <Button className="ml-4" variant="outline">
-                Sign In
+              Sign In
             </Button>
           </SignInButton>
         </SignedOut>
@@ -74,6 +83,6 @@ export function Navbar(): ReactElement {
       </div>
     </header>
   );
-};
+}
 
 export default Navbar;

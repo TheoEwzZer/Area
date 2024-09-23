00b5a46060f30b332fdd058c55c94314c2f8b8ef
Théo <theo.fabianomattei@gmail.com>
2024-09-23 11:43:38 +0200

    🏷️ refactor: extract blocktype type

--- app/create/types.ts
+++ app/create/types.ts
@@ -1,8 +1,10 @@
 import { ServiceType } from "@prisma/client";
 import { ReactNode } from "react";
 
+export type BlockType = "action" | "reaction";
+
 export interface Block {
-  type: "action" | "reaction";
+  type: BlockType;
   text: string;
   icon: ReactNode;
   service?: ServiceType;

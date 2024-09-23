c33e254d0c58ff0a95601f7e355940b91ab969e4
Théo <theo.fabianomattei@gmail.com>
2024-09-23 12:11:59 +0200

    🛂 fix: update Discord authorization scope

--- app/api/oauth2/authorize/DISCORD/route.ts
+++ app/api/oauth2/authorize/DISCORD/route.ts
@@ -4,7 +4,7 @@ export async function GET(): Promise<NextResponse<unknown>> {
   const authorizationUrl = "https://discord.com/api/oauth2/authorize";
   const clientId: string = process.env.DISCORD_CLIENT_ID!;
   const redirectUri = `${process.env.NEXT_PUBLIC_BASE_URL}/api/oauth2/callback/DISCORD`;
-  const scope = "identify";
+  const scope = "identify bot applications.commands guilds";
 
   const url = `${authorizationUrl}?client_id=${clientId}&redirect_uri=${encodeURIComponent(
     redirectUri

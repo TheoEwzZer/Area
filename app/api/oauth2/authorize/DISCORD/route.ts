413089639e3482bc1ae2496e8bb161401b7a7cf7
Théo <theo.fabianomattei@gmail.com>
2024-09-23 12:15:04 +0200

    🛂 fix: add bot permissions

--- app/api/oauth2/authorize/DISCORD/route.ts
+++ app/api/oauth2/authorize/DISCORD/route.ts
@@ -5,10 +5,11 @@ export async function GET(): Promise<NextResponse<unknown>> {
   const clientId: string = process.env.DISCORD_CLIENT_ID!;
   const redirectUri = `${process.env.NEXT_PUBLIC_BASE_URL}/api/oauth2/callback/DISCORD`;
   const scope = "identify bot applications.commands guilds";
+  const permissions = 134160;
 
   const url = `${authorizationUrl}?client_id=${clientId}&redirect_uri=${encodeURIComponent(
     redirectUri
-  )}&response_type=code&scope=${scope}`;
+  )}&response_type=code&scope=${scope}&permissions=${permissions}`;
 
   return NextResponse.redirect(url);
 }

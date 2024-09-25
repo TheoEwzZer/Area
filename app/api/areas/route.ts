40f3430ba0c87e8ccaabde45c1c9cb5763e6a7de
Théo <theo.fabianomattei@gmail.com>
2024-09-25 16:59:41 +0200

    🐛 fix: fix service and reaction verification

--- app/api/areas/route.ts
+++ app/api/areas/route.ts
@@ -1,6 +1,11 @@
 import { db } from "@/lib/db";
 import { currentUser, User } from "@clerk/nextjs/server";
-import { Action, Area, Reaction } from "@prisma/client";
+import {
+  Area,
+  ServiceAction,
+  ServiceInfo,
+  ServiceReaction,
+} from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest): Promise<
@@ -14,13 +19,38 @@ export async function POST(req: NextRequest): Promise<
   const user: User | null = await currentUser();
 
   try {
-    const action: Action | null = await db.action.findFirst({
-      where: { service: actionService, name: actionName },
+    const actionServiceInfo: ServiceInfo | null =
+      await db.serviceInfo.findUnique({
+        where: { type: actionService },
+      });
+
+    const reactionServiceInfo: ServiceInfo | null =
+      await db.serviceInfo.findUnique({
+        where: { type: reactionService },
+      });
+
+    if (!actionServiceInfo || !reactionServiceInfo) {
+      return NextResponse.json(
+        { detail: "Service information not found" },
+        { status: 404 }
+      );
+    }
+
+    const action: ServiceAction | null = await db.serviceAction.findFirst({
+      where: {
+        serviceInfoId: actionServiceInfo.id,
+        name: actionName,
+      },
     });
 
-    const reaction: Reaction | null = await db.reaction.findFirst({
-      where: { service: reactionService, name: reactionName },
-    });
+    const reaction: ServiceReaction | null = await db.serviceReaction.findFirst(
+      {
+        where: {
+          serviceInfoId: reactionServiceInfo.id,
+          name: reactionName,
+        },
+      }
+    );
 
     if (!action || !reaction) {
       return NextResponse.json(

eb25c33f6700dccfb856a7649118bcb72fc86e0a
Théo <theo.fabianomattei@gmail.com>
2024-10-05 15:49:04 +0200

    🛂 fix: update GMAIL authorization route to include additional scopes

--- website/app/api/oauth2/authorize/GMAIL/route.ts
+++ website/app/api/oauth2/authorize/GMAIL/route.ts
@@ -4,11 +4,16 @@ export async function GET(): Promise<NextResponse<unknown>> {
   const authorizationUrl = "https://accounts.google.com/o/oauth2/v2/auth";
   const clientId: string = process.env.GOOGLE_CLIENT_ID!;
   const redirectUri = `${process.env.NEXT_PUBLIC_BASE_URL}/api/oauth2/callback/GMAIL`;
-  const scope = "https://mail.google.com/";
+  const scopes: string[] = [
+    "https://mail.google.com/",
+    "https://www.googleapis.com/auth/gmail.send",
+    "https://www.googleapis.com/auth/gmail.compose",
+    "https://www.googleapis.com/auth/gmail.modify",
+  ];
 
   const url = `${authorizationUrl}?client_id=${clientId}&redirect_uri=${encodeURIComponent(
     redirectUri
-  )}&response_type=code&scope=${encodeURIComponent(scope)}&access_type=offline&prompt=consent`;
+  )}&response_type=code&scope=${encodeURIComponent(scopes.join(" "))}&access_type=offline&prompt=consent`;
 
   return NextResponse.redirect(url);
 }
